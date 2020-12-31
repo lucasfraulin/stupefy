5ff9420c18549c432cf184eba349b227c8e7e172
Lucas Fraulin <lfraulin@uwo.ca>
2020-12-31 14:56:59 -0500

    finish nowplaying bar

--- index.php
+++ index.php
@@ -18,9 +18,120 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stupefy</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Eagle+Lake&family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet">
+
 </head>
 <body>
-    hello
+
+  <div id="mainContainer" class="">
+
+    <!--top container -->
+    <div id="topContainer" >
+
+        <!--navbar container -->
+        <div id="navbarContainer" class="vh-100">
+
+          <nav class="navBar">
+
+            <a href="index.php" class="logo">
+              <img src="assets/images/houses/slytherin.png" style="height: 100px; width: 100px;" alt="logo">
+            </a>
+
+          </nav>
+
+        </div>
+
+    </div>
+
+
+    <!-- footer bar -->
+    <div id="nowPlayingContainerBar" class="container-fluid">
+      <div id="nowPlayingBar" class="row ">
+
+        <div id="nowPlayingLeft" class="col-4">
+
+          <div class="content">
+            <span class="albumLink">
+              <img src="assets/images/square.png" alt="" class="album-footer-img">
+            </span>
+
+            <div class="trackInfo container-fluid">
+              <span class="trackName row">
+                <span>Do The Hippogriff</span>
+              </span>
+              <span class="artistName row">
+                <span>Weird Sisters</span>
+              </span>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div id="nowPlayingCenter" class="col-4">
+
+          <div class="content player-controls">
+
+            <div class="buttons">
+              <button class="control-button shuffle" title="shuffle button">
+                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+              </button>
+              <button class="control-button previous" title="previous button">
+                <img src="assets/images/icons/previous.png" alt="previous">
+              </button>
+              <button class="control-button play" title="play button">
+                <img src="assets/images/icons/play.png" alt="play">
+              </button>
+              <button class="control-button pause" title="pause button" style="display:none;">
+                <img src="assets/images/icons/pause.png" alt="pause">
+              </button>
+              <button class="control-button next" title="next button">
+                <img src="assets/images/icons/next.png" alt="next">
+              </button>
+              <button class="control-button repeat" title="repeat button">
+                <img src="assets/images/icons/repeat.png" alt="repeat">
+              </button>
+            </div>
+
+            <div class="playbackBar">
+              <span class="progressTime current">0.00</span>
+
+              <div class="progressBar">
+                <div class="progressBarBg">
+                  <div class="myprogress"></div>
+                </div>
+              </div>
+
+              <span class="progressTime remaining">0.00</span>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div id="nowPlayingRight" class="col-4">
+          <div class="volumeBar">
+
+            <button class="control-button volume">
+              <img src="assets/images/icons/volume.png" alt="">
+            </button>
+
+            <div class="progressBar">
+              <div class="progressBarBg">
+                <div class="myprogress"></div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
 </body>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--- register.php
+++ register.php
@@ -23,87 +23,121 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Stupefy!</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Eagle+Lake&family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/css/register.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="assets/js/register.js"></script>
+
 </head>
 <body>
-  <div id="background" class="vh-100">
+  <?php
+  if(isset($_POST['registerButton'])){
+    echo '<script>
+            $(document).ready(function(){
+              $("#loginForm").hide();
+              $("#registerForm").show();
+            });
+            </script>';
+  } else {
+    echo '<script>
+            $(document).ready(function(){
+              $("#registerForm").hide();
+              $("#loginForm").show();
+            });
+            </script>';
+  }
 
-    <div class="row">
+   ?>
 
-      <div class="col-md-7 text-center align-middle">
-        <h1 class="register-title">Welcome to Stupefy</h1>
+
+
+
+  <div id="background" class="vh-100 m-0">
+
+    <div class="row m-auto container-fluid">
+
+      <div class="col-md-6 m-auto">
+        <h1 class="register-title text-center"><span class="smaller-register-title">Welcome to</span> <br/>Stupefy</h1>
       </div>
 
-      <div id="inputContainer" class="col-md-5">
+      <div id="inputContainer" class="col-md-6 vh-100 container-fluid">
 
-          <form id="loginForm" class="login-form" action="register.php" method="POST">
-              <h2>Login to your account</h2>
+          <form id="loginForm" class="login-form text-md-left vertical-center" action="register.php" method="POST">
+              <h2 class="form-title">Login to your account</h2>
 
                 <div class="form-group">
-                  <?php echo $account->getError(Constants::$loginFailed); ?>
-                  <label for="loginUsername" class="text-white-50">Username: </label>
+                  <span class="errorMessage"><?php echo $account->getError(Constants::$loginFailed); ?></span>
+                  <label for="loginUsername" class="text-white">Username: </label>
                   <input id="loginUsername" class="form-control" name="loginUsername" type="text" value="<?php getValue('loginUsername'); ?>" placeholder="UserName" required>
                 </div>
 
                 <div class="form-group">
-                  <label for="loginPassword" class="text-white-50">Password: </label>
+                  <label for="loginPassword" class="text-white">Password: </label>
                   <input id="loginPassword" class="form-control"  name="loginPassword" type="password" placeholder="Your Password" value="<?php getValue('loginPassword'); ?>" required>
                 </div>
 
-              <button class="btn btn-primary" type="submit" name="loginButton">Login</button>
+              <button class="btn btn-lg btn-block" type="submit" name="loginButton">Login</button>
+
+              <div class="hasAccountText">
+                <span id="hideLogin">Haven't been sorted into a house yet? Signup here.</span>
+              </div>
           </form>
 
 
-          <form id="registerForm" class="register-form" action="register.php" method="POST">
-              <h2>Register your account</h2>
+          <form id="registerForm" class="register-form vertical-center collapse" action="register.php" method="POST">
+              <h2 class="form-title">Register your account</h2>
               <div class="form-group">
-                <span><?php echo $account->getError(Constants::$usernameWrongLength); ?></span>
-                <span><?php echo $account->getError(Constants::$usernameTaken); ?></span>
-                <label class="text-white-50" for="registerUsername">Username: </label>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$usernameWrongLength); ?></span>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$usernameTaken); ?></span>
+                <label class="text-white" for="registerUsername">Username: </label>
                 <input id="registerUsername" class="form-control" name="registerUsername" type="text" value="<?php getValue('registerUsername'); ?>" placeholder="UserName" required>
               </div>
               <div class="form-group">
-                <?php echo $account->getError(Constants::$firstNameWrongLength); ?>
-                <label class="text-white-50" for="registerFirstName">First Name: </label>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$firstNameWrongLength); ?></span>
+                <label class="text-white" for="registerFirstName">First Name: </label>
                 <input id="registerFirstName" class="form-control" type="text" name="registerFirstName" value="<?php getValue('registerFirstName'); ?>" placeholder="e.g. Bob">
               </div>
               <div class="form-group">
-                <?php echo $account->getError(Constants::$lastNameWrongLength); ?>
-                <label class="text-white-50" for="registerLastName">Last Name: </label>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$lastNameWrongLength); ?></span>
+                <label class="text-white" for="registerLastName">Last Name: </label>
                 <input id="registerLastName" class="form-control" type="text" name="registerLastName"  value="<?php getValue('registerLastName'); ?>" placeholder="e.g. Smith">
               </div>
               <div class="form-group">
-                <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
-                <?php echo $account->getError(Constants::$emailInvalid); ?>
-                <?php echo $account->getError(Constants::$emailTaken); ?>
-                <label class="text-white-50" for="registerEmail">Email: </label>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$emailsDoNotMatch); ?></span>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$emailInvalid); ?></span>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$emailTaken); ?></span>
+
+                <label class="text-white" for="registerEmail">Email: </label>
                 <input id="registerEmail" class="form-control" type="email" name="registerEmail" value="<?php getValue('registerEmail'); ?>" placeholder="e.g. user@email.com">
               </div>
               <div class="form-group">
-                <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
-                <?php echo $account->getError(Constants::$emailInvalid); ?>
-                <?php echo $account->getError(Constants::$emailTaken); ?>
-                <label class="text-white-50" for="registerEmailConfirm">Confirm Email: </label>
+
+                <label class="text-white" for="registerEmailConfirm">Confirm Email: </label>
                 <input id="registerEmailConfirm" class="form-control" type="email" name="registerEmailConfirm" value="<?php getValue('registerEmailConfirm'); ?>" placeholder="e.g. user@email.com">
               </div>
               <div class="form-group">
-                <?php echo $account->getError(Constants::$passwordsDoNotMatch);?>
-                <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
-                <?php echo $account->getError(Constants::$passwordTooShort); ?>
-                <label class="text-white-50" for="registerPassword">Password: </label>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$passwordsDoNotMatch);?></span>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?></span>
+                <span class="errorMessage"><?php echo $account->getError(Constants::$passwordTooShort); ?></span>
+
+                <label class="text-white" for="registerPassword">Password: </label>
                 <input id="registerPassword" class="form-control" name="registerPassword" value="<?php getValue('registerPassword'); ?>" type="password" placeholder="Your password" required>
               </div>
               <div class="form-group">
-                <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
-                <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
-                <?php echo $account->getError(Constants::$passwordTooShort); ?>
-                <label class="text-white-50" for="registerPasswordConfirm">Confirm Password: </label>
+
+                <label class="text-white" for="registerPasswordConfirm">Confirm Password: </label>
                 <input id="registerPasswordConfirm" class="form-control" name="registerPasswordConfirm" value="<?php getValue('registerPasswordConfirm'); ?>" type="password" placeholder="Confirm Password" required>
               </div>
 
-              <button class="btn btn-primary" type="submit" name="registerButton">Register</button>
+              <button class="btn btn-lg btn-block" type="submit" name="registerButton">Register</button>
+
+              <div class="hasAccountText">
+                <span id="hideRegister">Already in a house? Login here.</span>
+              </div>
           </form>
       </div>
     </div>
